224dcf3b3ca47ec582564c4c5ebd7ce3f176c096
Dylan Madisetti <dylan.madisetti@gmail.com>
2019-11-19 14:16:22 -0500

    undo
    s

--- 1.1.0/entrypoint.sh
+++ 1.1.0/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 WORKING_DIRECTORY="${INPUT_WORKING_DIRECTORY:-./}"
-export CC_COMPILE="${INPUT_CC_COMPILE:-gcc}"
-export CC="CC_COMPILE"
+# export CC_COMPILE="${INPUT_CC_COMPILE:-gcc}"
+# export CC="CC_COMPILE"
 env
 
 cd "${WORKING_DIRECTORY}";
